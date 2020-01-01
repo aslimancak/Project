a0fc133e090c1f2ca672c10f67ea5feb410265c9
aslimancak <55581428+aslimancak@users.noreply.github.com>
2020-01-01 17:27:58 +0300

    some adjustments made on Appointment page

--- Project/AppointmentSystem/AppointmentWindow.xaml.cs
+++ Project/AppointmentSystem/AppointmentWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AppointmentSystem.Data;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AppointmentSystem
@@ -17,9 +21,73 @@ namespace AppointmentSystem
     /// </summary>
     public partial class AppointmentWindow : Window
     {
+        AppointmentSystemDB db = new AppointmentSystemDB();
         public AppointmentWindow()
         {
             InitializeComponent();
+
+            foreach (Department department in db.Courses)
+            {
+                departmantComboBox.Items.Add(department.DepartmentName);
+            }
+
         }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            titleComboBox.Items.Clear();
+            if (departmantComboBox.SelectedIndex != -1)
+            {
+
+                if (departmantComboBox.Text == "Pediatrics")
+                {
+
+                    IQueryable<Doctor> doctors = db.Doctors.Where(x => x.DepartmentID == 1).Select(x => x);
+
+                    foreach (var x in doctors)
+                    {
+                        titleComboBox.Items.Add("Dr." + x.Name + " " + x.Surname);
+
+                    }
+                }
+
+                if (departmantComboBox.Text == "Gynaecology ")
+                {
+                    IQueryable<Doctor> doctors = db.Doctors.Where(x => x.DepartmentID == 2).Select(x => x);
+
+                    foreach (var x in doctors)
+                    {
+                        titleComboBox.Items.Add("Dr." + x.Name + " " + x.Surname);
+
+                    }
+
+                }
+                titleComboBox.Items.Refresh();
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var department = db.Courses.Where(x => x.DepartmentName == departmantComboBox.Text).FirstOrDefault();
+            var doctor = db.Doctors.Where(x => ("Dr." + x.Name + " " + x.Surname) == titleComboBox.Text).FirstOrDefault();
+
+
+
+            Appointment appointment = new Appointment();
+            appointment.DepartmentID = department.ID;
+            appointment.DoctorID = doctor.ID;
+            appointment.Timestamp = dtpAppointmentDate.SelectedDate.Value;
+            
+
+            db.Appointments.Add(appointment);
+
+            db.SaveChanges();
+            MessageBox.Show("Randevunuz başarıyla kaydedildi.");
+
+        }
+
+
+
     }
 }
